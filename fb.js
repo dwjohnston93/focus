4658ad902eca45e152bdec6d9094547f75d83868
dwjohnsto93 <dwjohnston93@gmail.com>
2019-06-24 17:31:51 -0700

    FB images rotate -FB images rotate through 3 saved images with credit to each image photographer

--- fb.js
+++ fb.js
@@ -1,17 +1,37 @@
+//attributions to inspirational images used
+let credits = ["Alex Haney", "Joshua Earle", "Malcolm Lightbody"]
+
+//grab fb's feed element and parent, and remove the feed
 let fbFeed = document.getElementById('content_container')
 let fbFeedParent =  document.getElementById('content_container').parentNode
-
 fbFeed.parentNode.removeChild(fbFeed)
 
-let imageNum = 0;
+var imageNum = 0;
+function rotateImage(){
+    if(imageNum === 2){
+        imageNum = -1; 
+    }
+    imageNum++
+    console.log("imageNum:", imageNum)
+    document.getElementById('fbImg').src = chrome.runtime.getURL(`images/facebook${imageNum}.jpg`)
+    document.getElementById('fbCredit').innerHTML = `Photo by ${credits[imageNum]} on Unsplash`
+}
 
-let txtTag= document.createElement('div')
-txtTag.innerHTML = 'get that job!'
-txtTag.className = 'imgTxt'
-fbFeedParent.appendChild(txtTag)
+//create inspirational quote and image crediting elements dynamically
+let quote= document.createElement('h2')
+quote.innerHTML = 'get that job!'
+quote.className = 'quote'
+let credit = document.createElement('h6')
+credit.id = 'fbCredit'
+credit.innerHTML = `Photo by ${credits[imageNum]} on Unsplash`
+fbFeedParent.appendChild(quote)
+fbFeedParent.appendChild(credit)
 
 let imgTag = document.createElement('img')
 let imageURL = chrome.runtime.getURL(`images/facebook${imageNum}.jpg`)
 imgTag.src = imageURL
 imgTag.className = 'img'
+imgTag.id = 'fbImg'
 fbFeedParent.appendChild(imgTag)
+
+document.addEventListener("click", rotateImage)
